4a9e46fc0ab2c3a6f01a4dc9c4241f1e59fb7eac
David Cabo <david.cabo@gmail.com>
2017-01-13 11:38:04 +0100

    Update execution data to 2016M11

--- static/javascripts/theme.js
+++ static/javascripts/theme.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 
   var addChartsAlert = function(selector){
     var str = {
-      'es': 'Datos actualizados a 31 de octubre de 2016',
-      'en': 'Data up to 31st October 2016'
+      'es': 'Datos actualizados a 30 de noviembre de 2016',
+      'en': 'Data up to 30th November 2016'
     };
     var cont = $(selector);
     if( cont.size() > 0 ){
