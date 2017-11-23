a453d0c32dfa724ae6c03d07e1643eb82963ef3c
David Cabo <david.cabo@gmail.com>
2017-11-23 01:49:27 +0100

    Classify investments by line (using policies) civio/presupuesto-management#527

--- loaders/madrid_investments_loader.py
+++ loaders/madrid_investments_loader.py
@@ -6,7 +6,8 @@ import re
 
 class MadridInvestmentsLoader(InvestmentsLoader):
 
-    # An artifact of the in2csv conversion of the original XLS files is a trailing '.0', which we remove here
+    # An artifact of the in2csv conversion of the original XLS files is a trailing '.0',
+    # which we remove here
     def clean(self, s):
         return s.split('.')[0]
 
@@ -23,17 +24,24 @@ class MadridInvestmentsLoader(InvestmentsLoader):
         if int(year) < 2017:
             project_id = line[3]
             description = line[4]
-            gc_code = line[0]
+            investment_line = self.clean(line[1])
+            gc_code = self.clean(line[0]).strip()
             amount = line[5]
         else:
             project_id = line[7]
             description = unicode(line[8], encoding='iso-8859-1').encode('utf8')
+            investment_line = line[11]
             gc_code = line[9]
             amount = line[28 if is_actual else 23]
 
+        # Note we implement the investment lines as an extension of functional policies.
+        # See #527 for further information.
         return {
             'is_actual': is_actual,
-            'gc_code': self.clean(gc_code).strip(),
+            'gc_code': gc_code,
+            'fc_code': 'X'+investment_line,
+            'fc_area': 'X',
+            'fc_policy': 'X'+investment_line,
             'amount': self.parse_amount(amount),
             'project_id': project_id.strip(),
             'description': project_id.strip() + ' ' + self._titlecase(description)
