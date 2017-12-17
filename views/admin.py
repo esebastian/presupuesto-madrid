ad19588035f03711de1e9c57c3579a0e6dcd0b85
David Cabo <david.cabo@gmail.com>
2017-12-17 02:24:06 +0100

    Add sample output to admin page. civio/presupuesto-management#567

--- views/admin.py
+++ views/admin.py
@@ -2,8 +2,12 @@
 from coffin.shortcuts import render_to_response
 from budget_app.views.helpers import *
 
+import glob
+
 def admin(request, render_callback=None):
     # Get request context
     c = get_context(request, css_class='body-admin', title='')
+
+    c['output'] = glob.glob("*.*")
 
     return render_to_response('admin/index.html', c)
