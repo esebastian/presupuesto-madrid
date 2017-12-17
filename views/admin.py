d683d224753e067629a26aaf3c3fc789f30ecb15
David Cabo <david.cabo@gmail.com>
2017-12-17 02:44:54 +0100

    Try listing files in app root

--- views/admin.py
+++ views/admin.py
@@ -8,6 +8,6 @@ def admin(request, render_callback=None):
     # Get request context
     c = get_context(request, css_class='body-admin', title='')
 
-    c['output'] = glob.glob("*.*")
+    c['output'] = glob.glob(ROOT_PATH+"/*.*")
 
     return render_to_response('admin/index.html', c)
