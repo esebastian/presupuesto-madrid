12bb429638963f8826a18dbf1d42b7536d2a36ea
David Cabo <david.cabo@gmail.com>
2018-02-27 20:31:36 +0100

    Add payment data 2011-2016 civio/presupuesto-management#601

--- loaders/madrid_payments_loader.py
+++ loaders/madrid_payments_loader.py
@@ -1,15 +1,27 @@
 # -*- coding: UTF-8 -*-
 
+import datetime
+import re
+
 from budget_app.loaders import PaymentsLoader
 from budget_app.models import Budget
-import datetime
 
 class MadridPaymentsLoader(PaymentsLoader):
   # Parse an input line into fields
   def parse_item(self, budget, line):
     # But what we want as area is the programme description
-    policy_id = line[0][:2]
+    policy_id = line[1][:2]
+    print policy_id
     policy = Budget.objects.get_all_descriptions(budget.entity)['functional'][policy_id]
+
+    # Some descriptions are missing in early years
+    description = line[2].strip()
+    if description=="":
+      description="..."
+
+    # And some payee names have some trailing punctuation marks: one or two instances of " ."
+    payee = line[3].strip()
+    payee = re.sub(r'( \.)+$', '', payee)
 
     return {
       'area': policy,
@@ -17,7 +29,7 @@ class MadridPaymentsLoader(PaymentsLoader):
       'ec_code': None,
       'date': None,
       'contract_type': None,
-      'payee': line[2].strip(),
-      'description': line[1].strip(),
-      'amount': self._read_english_number(line[3])
+      'payee': payee,
+      'description': description,
+      'amount': self._read_english_number(line[4])
     }
