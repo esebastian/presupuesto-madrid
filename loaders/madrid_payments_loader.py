748bda72e501220efdcf5201343c8f7dcc011990
David Cabo <david.cabo@gmail.com>
2018-04-03 01:16:24 +0200

    Populate institutional category when loading payments. This can be done thanks to civio/presupuesto-management#677 now, but is part of civio/presupuesto-management#601

--- loaders/madrid_payments_loader.py
+++ loaders/madrid_payments_loader.py
@@ -22,12 +22,23 @@ class MadridPaymentsLoader(PaymentsLoader):
     payee = line[3].strip()
     payee = re.sub(r'( \.)+$', '', payee)
 
+    # Get institutional code. See the budget_loader for more details on this process for Madrid.
+    institution = self.get_institution_code(line[0][0:3])
+    ic_code = institution + (line[0][3:6] if institution=='0' else '00')
+
     return {
       'area': policy,
       'fc_code': None,
       'ec_code': None,
+      'ic_code': ic_code,
       'date': None,
       'payee': payee,
       'description': description + ' (' + str(budget.year) + ')',
       'amount': self._read_english_number(line[4])
     }
+
+  # We expect the organization code to be one digit, but Madrid has a 3-digit code.
+  # We can _almost_ pick the last digit, except for one case.
+  def get_institution_code(self, madrid_code):
+      institution_code = madrid_code if madrid_code!='001' else '000'
+      return institution_code[2]
