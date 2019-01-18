b93c8e7fc088fd8ad228a60b7cafd947868d45cd
Eduardo Sebastian <esebastian@altiora.es>
2019-01-18 19:07:16 +0100

    Add page title to 'Visita guiada' section
    civio/presupuesto-management#834

--- views/guidedvisit.py
+++ views/guidedvisit.py
@@ -1,12 +1,13 @@
 # -*- coding: UTF-8 -*-
-from coffin.shortcuts import render_to_response
+
+from django.utils.translation import ugettext as _
 from budget_app.views.helpers import *
 
 def guidedvisit(request, render_callback=None):
     # Get request context
-    c = get_context(request, css_class='body-entities', title='')
+    c = get_context(request, css_class='body-entities', title=_(u'Visita guiada'))
 
     # Setup active_tab for menu options
     c['active_tab'] = 'guidedvisit'
 
-    return render_to_response('guidedvisit/index.html', c)
+    return render_response('guidedvisit/index.html', c)
